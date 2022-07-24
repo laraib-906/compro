8122167d2b4d7cf6d82cf7684bdc0607fba21e8f
laraib-906 <laraibamna906@gmail.com>
2022-07-25 02:04:24 +0500

    complete contact page

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,32 @@
 
         const loader = document.getElementById("preloader");
         window.addEventListener("load", () => {
-            setTimeout(() => {
-                loader.style.display = "none";
-            }, 1000);
+            loader.style.display = "none";
+            // setTimeout(() => {
+            // }, 300);
+        });
+
+        //top scroll handler
+
+        const scrollBtn = document.getElementById("top-button");
+
+        window.onscroll = function() {
+            scrollFunction();
+        };
+
+        function scrollFunction() {
+            if (
+                document.body.scrollTop > 20 ||
+                document.documentElement.scrollTop > 20
+            ) {
+                scrollBtn.style.display = "block";
+            } else {
+                scrollBtn.style.display = "none";
+            }
+        }
+
+        scrollBtn.addEventListener("click", () => {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
         });
     })();
